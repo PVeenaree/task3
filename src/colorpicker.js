1c9834af4447a9f192ee85ab2af7a3cf128f83b1
PVeenaree <veenasree.2020@vitstudent.ac.in>
2023-06-19 16:59:30 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/colorpicker.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './color.css';
+const ColorPicker = ({ colors }) => {
+  const [showColors, setShowColors] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const handleButtonClick = () => {
+    setShowColors(!showColors);
+  };
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
+    setShowColors(false);
+  };
+
+  return (
+    <div>
+      <button button className = "color-btn"
+        style={{ backgroundColor: selectedColor }}
+        onClick={handleButtonClick}
+      >
+        Pick a color
+      </button>
+      {showColors && (
+              <div className="colordiv">
+                  <ul className="color-list">
+          {colors.map((color, index) => (
+            <li
+              key={index}
+              className="color-item"
+              style={{ backgroundColor: color }}
+              onClick={() => handleColorClick(color)}
+            ></li>
+          ))}
+        </ul>
+        </div>
+      )}
+      {selectedColor && (
+        <div className="colorName">
+          <p>You selected: {selectedColor}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ColorPicker;
