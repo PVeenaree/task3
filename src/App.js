5980f2b934e6dac7afe8c4190ab4c1fa0657ff0d
PVeenaree <veenasree.2020@vitstudent.ac.in>
2023-06-20 15:40:13 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ColorPicker from './components/colorpicker';
+import ColorPicker from './colorpicker';
 
 const App = () => {
   const colors = ['Violet', 'Indigo', 'Blue', 'Green', 'Yellow', 'Orange', 'Red','Aqua','Salmon','Lime','Magenta','Midnightblue'];
